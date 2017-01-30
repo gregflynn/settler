c8aea7b7fc09c743f4fc8f6d6be77b94381a51ed
Greg Flynn <gregflynn42@gmail.com>
2017-01-29 20:37:41 -0500

    bump version for bugfix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 config = {
     'name': 'settler',
-    'version': '0.1.1',
+    'version': '0.1.2',
     'author': 'Greg Flynn',
     'author_email': 'gregflynn42@gmail.com',
     'url': 'https://github.com/gregflynn/settler'
