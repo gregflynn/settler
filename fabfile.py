80ee3c8338e834960c10a53180d286c762c63052
Greg Flynn <gregflynn42@gmail.com>
2017-02-28 17:24:16 -0500

    make fabric tasks

---
+++ fabfile.py
@@ -0,0 +1,23 @@
+from fabric.api import task, local, lcd
+
+
+@task(default=True)
+def tasks():
+    local('fab -l')
+
+
+@task
+def install():
+    local('pip install -e .')
+    local('pip install -r test_requirements.txt')
+
+
+@task
+def unit_tests():
+    local('nosetests tests/')
+
+
+@task
+def integration_tests():
+    with lcd('integration_tests'):
+        local('python run_test.py')
