492cadf4e8d62d8c8da2e4b52f624a22a758a655
Greg Flynn <greg@gfly.io>
2023-03-27 22:15:59 -0500

    fix required version of sqlalchemy

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 
 setup(
     name='settler',
-    version='0.5.1',
+    version='0.5.2',
     author='Greg Flynn',
     author_email='gregflynn42@gmail.com',
     url='https://github.com/gregflynn/settler',
     packages=['settler'],
     provides=['settler'],
     install_requires=[
-        'sqlalchemy>=1.4'
+        'sqlalchemy>=2.0'
     ]
 )
